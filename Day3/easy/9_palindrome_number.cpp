cf353b3a8e87a8c5db9df861fa291f04b7ba7f7b
AtCloudDiwash <diwash.kuskusmiya@gmail.com>
2026-02-16 20:28:41 -0600

    palindrome number

---
+++ Day3/easy/9_palindrome_number.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        int half_reversed = 0;
+
+        if( x >= 0 && x < 10){
+            return true;
+        }
+
+        if(x < 0 || x%10 == 0){
+            return false;
+        }
+
+        while (x > half_reversed) {
+            half_reversed = half_reversed * 10 + x % 10;
+            x /= 10;
+        }
+
+        return x == half_reversed || x == half_reversed / 10;
+        
+    }
+};
